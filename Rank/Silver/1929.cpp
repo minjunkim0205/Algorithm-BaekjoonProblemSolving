ee8d33621c85947a2809aa34ca8dd346402fad77
minjunkim0205 <minjunkim0205@gmail.com>
2024-07-21 20:51:53 +0900

    PS 1929

--- Rank/Silver/1929.cpp
+++ Rank/Silver/1929.cpp
@@ -1,25 +1,27 @@
-//
-// Created by mjk on 2024-04-01.
-//
 #include <iostream>
-#include <vector>
-/* 
- * [ 소수 판별법 ]
- * - 에라토스테네스의 체 
- */
+
 using namespace std;
 /*Main*/
-int main(){
+int main()
+{
     // Input
-    int m, n; cin >> m >> n; // 1 <= M <= N <= 1,000,000
-    // Solve
-    bool flag[1000000] = {false, };
-    for (int i = 2; i <= n; ++i) { // 일단 1~n 까지
-        if ()
+    int m, n;
+    cin >> m >> n; // 1 <= M <= N <= 1,000,000
+    // Solve[sieve of Eratosthenes(에라토스테네스의 체)]
+    bool flag[1000000 + 1] = {false,};
+    flag[1] = true;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 2; i * j <= n; j++)
+        {
+            flag[i * j] = true;
+        }
     }
     // Ouput
-    for (int i = 1; i <= n; ++i) {
-        if (!flag[i]){
+    for (int i = m; i <= n; i++)
+    {
+        if (!flag[i])
+        {
             cout << i << '\n';
         }
     }
